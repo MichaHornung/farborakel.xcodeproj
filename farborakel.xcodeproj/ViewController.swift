bd8713ad0fbeb6d63fed6098c359c5db75005354
MichaHornung <michaelhornung2022@icloud.com>
2022-10-19 11:55:49 +0200

    Aufgabe 19.10.2022 erledigt

--- farborakel.xcodeproj/ViewController.swift
+++ farborakel.xcodeproj/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         kreis.layer.cornerRadius = 100
+        hintergund.layer.cornerRadius = 10
+        farbekreis.layer.cornerRadius = 10
     }
     
     @IBAction func changeBackground(_ sender: Any) {
